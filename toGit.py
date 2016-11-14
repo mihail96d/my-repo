edefb84aeb81c2773141ce0199617c98ff5e42f6
mihail96d <mihail96d@gmail.com>
2016-11-14 12:28:49 +0300

    third commit

--- toGit.py
+++ toGit.py
@@ -1,6 +1,8 @@
-print("Hi, GIT world!")
-# some words
+print("Hi, git world!")
 print("Here is a loop: ")
 for i in range(6): print(i)
 print("The loop has ended.")
-print("I have added some lines.")
+
+print("Another loop: ")
+for i in range(16): print(i)
+print("Second loop has ended.")
