263fefe25bc34aae1334abf53cecb0178da0cf9b
mihail96d <mihail96d@gmail.com>
2016-11-14 12:36:00 +0300

    Delete toGit.py

--- toGit.py
+++
@@ -1,8 +0,0 @@
-print("Hi, git world!")
-print("Here is a loop: ")
-for i in range(6): print(i)
-print("The loop has ended.")
-
-print("Another loop: ")
-for i in range(16): print(i)
-print("Second loop has ended.")
